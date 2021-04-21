671807601d91da431efd0e92eda981cf903b9fd0
Ivan Perrone <ivan.perrone@it.ey.com>
2021-04-21 16:37:22 +0200

    fix url

--- src/views/Certificate.vue
+++ src/views/Certificate.vue
@@ -169,7 +169,7 @@ export default {
         next(() => {
             store.dispatch('setLoading', true).then(() => {
                 if (to.params.txHash) {
-                     let apiUrl = 'http://13.93.119.145/transactions/'
+                     let apiUrl = 'https://blockchain.casagirelli.it/lavis/transactions/'
                      console.log(apiUrl)
                     axios.get(apiUrl +  to.params.txHash).then(response => {
                         // axios.get('/txResponse.json').then(response => {
